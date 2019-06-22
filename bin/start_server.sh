d57c009aa3fc5a740ac865ea07a4325e824e0a76
James Ding <james.ding.illinois@gmail.com>
2019-06-22 15:21:26 -0500

    start_server.sh update

---
+++ bin/start_server.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+docker_file="Dockerfile"
+docker_compose_file="docker-compose.yml"
+
+gradle clean build || ./gradlew clean build;
+docker build -t avengers/backend:1.0-SNAPSHOT .
+docker-compose up
+
+if [ -f $docker_compose_file ]; then
+    gawk '
+    BEGIN{FS=" "}
+    {
+        if ($0 ~ "80") {
+            print "backend service stopped at localhost:"$NF
+        } else if ($0 ~ /[0-9]:[0-9]+$/) {
+            print "database stopped at localhost:"$NF
+            exit;
+        } else {
+            next;
+        }
+    }
+    END{}' < $docker_compose_file
+fi
