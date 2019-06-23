dd349ae9b99df5a61b2cca2f31b2bcdbfcf3cd30
James Ding <james.ding.illinois@gmail.com>
2019-06-23 11:43:13 -0500

    Tag dto updated

--- src/main/java/avengers/backend/dto/jpa/Tag.java
+++ src/main/java/avengers/backend/dto/jpa/Tag.java
@@ -22,6 +22,12 @@ public class Tag {
     @Column(name = "tag_name")
     private String tagName;
 
+    @Column(name = "topic_type")
+    private String topicType;
+
+    @Column(name = "topic_group")
+    private String topicGroup;
+
     @JsonProperty("tag_id")
     public UUID getTagId() { return tagId; }
     @JsonProperty("tag_id")
@@ -36,4 +42,14 @@ public class Tag {
     public String getTagName() { return tagName; }
     @JsonProperty("tag_name")
     public void setTagName(String tagName) { this.tagName = tagName; }
+
+    @JsonProperty("topic_type")
+    public String getTopicType() { return topicType; }
+    @JsonProperty("topic_type")
+    public void setTopicType(String topicType) { this.topicType = topicType; }
+
+    @JsonProperty("topic_group")
+    public String getTopicGroup() { return topicGroup; }
+    @JsonProperty("topic_group")
+    public void setTopicGroup(String topicGroup) { this.topicGroup = topicGroup; }
 }
